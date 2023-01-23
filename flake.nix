9a4d5a2a29d72639c94b6c9a8bfa46d5ee0a3e8b
Michał Mieszczak <michal@mieszczak.com.pl>
2023-01-23 20:21:12 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,54 +1,50 @@
 {
   description = "Weight tracker";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-  inputs.poetry2nix = {
-    url = "github:nix-community/poetry2nix";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, nixpkgs, flake-utils }: {
+    # Nixpkgs overlay providing the application
+    overlay = nixpkgs.lib.composeManyExtensions [
+      (final: prev: {
+        # The application
+        weight-tracker = prev.poetry2nix.mkPoetryApplication {
+          projectDir = self;
+        };
+      })
+    ];
+  } // (flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      };
+    in
     {
-      # Nixpkgs overlay providing the application
-      overlay = nixpkgs.lib.composeManyExtensions [
-        poetry2nix.overlay
-        (final: prev: {
-          # The application
-          weight-tracker = prev.poetry2nix.mkPoetryApplication {
-            projectDir = ./.;
-          };
-        })
-      ];
-    } // (flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlay ];
-        };
-      in
-      {
-        apps = {
-          weight-tracker = pkgs.weight-tracker;
-        };
+      apps = {
+        inherit (pkgs) weight-tracker;
+      };
 
-        defaultApp = pkgs.weight-tracker;
+      defaultApp = pkgs.weight-tracker;
 
-        devShell =
-          let
-            pythonPackages = pkgs.python310Packages;
-          in
-          pkgs.mkShell {
-            buildInputs = with pythonPackages; [ python poetry debugpy venvShellHook ];
-            venvDir = "./.venv";
-            postVenvCreation = ''
-              unset SOURCE_DATE_EPOCH
-              poetry env use .venv/bin/python
-              poetry install
-            '';
-            postShellHook = ''
-              unset SOURCE_DATE_EPOCH
-            '';
-          };
-      }));
+      devShell =
+        let
+          pythonPackages = pkgs.python310Packages;
+        in
+        pkgs.mkShellNoCC {
+          buildInputs = with pythonPackages; [ python venvShellHook ];
+          packages = [ pkgs.poetry ];
+          venvDir = "./.venv";
+          postVenvCreation = ''
+            unset SOURCE_DATE_EPOCH
+            poetry env use .venv/bin/python
+            poetry install
+          '';
+          postShellHook = ''
+            unset SOURCE_DATE_EPOCH
+            poetry env info
+          '';
+        };
+    }));
 }
