6200e9f45fca0d3fbf8c1124e6ce14df881c2c91
Michał Mieszczak <m.mieszczak@piwik.pro>
2023-01-30 14:41:13 +0100

    Part 2

--- weight_tracker/schema.py
+++ weight_tracker/schema.py
@@ -9,6 +9,15 @@ class Record:
 
     def __post_init__(self):
         """Check field types."""
+        if isinstance(self.date, str):
+            self.date = datetime.date.fromisoformat(self.date)
+        if not isinstance(self.date, datetime.date):
+            raise TypeError
+
+        if isinstance(self.value, str):
+            self.value = float(self.value)
+        if not isinstance(self.value, int | float):
+            raise TypeError
 
     def to_dict(self):
         return {
